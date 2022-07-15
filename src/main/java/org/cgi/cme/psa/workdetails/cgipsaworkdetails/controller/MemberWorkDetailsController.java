e0c599754ddd07a948356c4f94e9384b2707cfde
Coollover108 <106576726+Coollover108@users.noreply.github.com>
2022-07-15 06:12:06 +0530

    Update MemberWorkDetailsController.java
    Reverting changes

--- src/main/java/org/cgi/cme/psa/workdetails/cgipsaworkdetails/controller/MemberWorkDetailsController.java
+++ src/main/java/org/cgi/cme/psa/workdetails/cgipsaworkdetails/controller/MemberWorkDetailsController.java
@@ -32,7 +32,7 @@ public class MemberWorkDetailsController {
    * 
    * Description: This method will allow user to fetch details of specific member PSA based of member ID.
    */
-  @GetMapping("/getMemberDetail") test for failure
+  @GetMapping("/getMemberDetail")
   private ResponseEntity<MemberDetailsDTO> getMemberDetail(@RequestParam int memberId) {
    // Demo for failure.....
     log.info("Retrieving member detais for give member ID"+memberId);
